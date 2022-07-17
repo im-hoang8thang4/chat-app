c5067620d882b090edc146369787c0cd4af7ec0f
Hoang ne <hoanghai08042001xxx@gmail.com>
2022-07-18 03:06:19 +0700

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from 'next'
+import Head from "next/head";
 import Sidebar from '../components/Sidebar'
 const Home: NextPage = () => {
   return (
    <div className='flex'>
+    <Head>
+        <title>Chat App</title>
+      </Head>
     <Sidebar />
    </div>
   )
